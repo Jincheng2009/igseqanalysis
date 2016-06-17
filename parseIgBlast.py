9dfe57be6d985a594f67fddb38194aecee95bd63
wuji <wuji@USMD1WLR9X1A7E.medimmune.com>
2016-06-17 16:51:06 -0400

    - Add scripts to parse IgBlast output

---
+++ parseIgBlast.py
@@ -0,0 +1,188 @@
+import sys
+import re
+import os
+from Bio import SeqIO
+import csv
+import getopt
+from sequtility import Alignment
+from sequtility import Sequence 
+
+def main(argv):
+    extractFastq=False
+    extractCoverage=False
+    readFromFile=False
+    try:
+        opts, args = getopt.getopt(argv,"h", ["blast=","fastq=","mutation=","coverage="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            usage()
+            sys.exit()
+        elif opt == "--fastq":
+            extractFastq=True
+            fastqfile = arg
+        elif opt == "--blast":
+            blastfile = arg
+            readFromFile=True
+        elif opt == "--mutation":
+            fileout = arg
+        elif opt == "--coverage":
+            extractCoverage=True
+            coverage_out = arg
+    
+    fileout = open(fileout, "wb")
+    writer = csv.writer(fileout)
+    # writer.writerow('id, germline, query_pos, ref_pos, query_base, ref_base, b3, b2, b1, a1, a2, a3, mismatch, length, gap, phred')
+    
+    if extractCoverage:
+        coveragefile = open(coverage_out, "w")
+                            
+    if extractFastq and os.path.isfile(fastqfile):
+        fastq_dict=SeqIO.index(fastqfile,"fastq")
+    
+    count = 0
+    printNext = 0
+    coverage = {}
+    strandPlus=True
+    
+    if readFromFile:
+        filein = open(blastfile)
+    else:
+        filein = sys.stdin
+
+    fastq_dict = {}
+    recordEnd=False
+    extractGermline=False
+    extractStrand=False
+    extractAlign=False
+    nafter=0
+    previous_line=""
+    for line in filein:
+        ## Not trim the end when parsing the alignment section
+        line = line.strip('\n')
+        if not extractAlign:
+            line=line.rstrip()
+        # line=line.strip()
+        if line.startswith("Query="):
+            align_record=None
+            inframe=True
+            recordEnd=False
+            extractGermline=False
+            extractStrand=False
+            extractAlign=False
+            nafter=0
+            vgene=""
+            jgene=""
+            dgene="" 
+            refs=[]
+            alns=[]
+            fastaid=line[7:].split(' ')[0]
+            quality=[]
+            count += 1
+            strandPlus = True
+            query_length = 0
+            if extractFastq and fastq_dict.has_key(fastaid):
+                quality = fastq_dict[fastaid].letter_annotations["phred_quality"]
+            if count >= printNext:
+                sys.stderr.write("Processed: " + str(count) + "\n")
+                printNext = printNext + 10000
+        
+        if line.startswith("Length="):
+            query_length = int(line[7:])
+        
+        # End of one alignment result        
+        if line.startswith("Effective search space used:"):
+            recordEnd=True
+            if align_record is not None:
+                for record in align_record.getMutations():
+                    outline = ""
+                    for item in record:
+                        outline = outline + "," + str(item)
+                    outline = outline[1:] + "\n"
+                    sys.stdout.write(outline)    
+    
+        ## Extract germline genes
+        if line.startswith("Sequences producing significant alignments"):
+            extractGermline=True
+        if line.startswith("Domain classification requested"):
+            extractGermline=False
+    
+        if extractGermline and line.startswith("IG"):
+            gene = line.split(' ')[0]
+            if gene.startswith("IGHV") or gene.startswith("IGLV"):
+                vgene = gene
+            elif gene.startswith("IGHD"):
+                dgene = gene
+            elif gene.startswith("IGHJ") or gene.startswith("IGLJ"):
+                jgene = gene
+                
+        ## Extract strand information
+        if extractStrand:
+            nafter =+ 1
+        if line.startswith("V-(D)-J rearrangement summary for query sequence"):
+            extractStrand=True
+            nafter=0
+        if nafter == 1 and extractStrand:
+            extractStrand=False
+            tokens = line.split('\t')
+            strand=tokens[-1]
+            frame=tokens[-2]
+            if strand=="+":
+                strandPlus=True
+            else:
+                strandPlus=False
+            if frame=="No":
+                inframe=False
+    
+        ## Extract alignment information
+        if extractAlign:
+            nafter=+1
+        if line=="Alignments" and inframe:
+            extractAlign=True
+            nafter=0
+            align_record = Alignment(fastaid, query_length)
+            query_seq = None
+            seq = None
+            dseq = None
+            jseq = None
+            astart = 0   
+        if extractAlign:
+            tokens=line.split(' ')
+            # remove empty strings
+            tokens=[x for x in tokens if x]
+            if len(tokens)>0 and re.match("(lcl\|)?Query_", tokens[0]):
+                if query_seq is None:
+                    query_seq = Sequence(tokens[2], tokens[1], tokens[3], fastaid, strandPlus)
+                    align_record.setQuery(query_seq)
+                    astart = 0
+                else:
+                    astart = len(query_seq.getSequence())
+                    query_seq.addSequence(tokens[2], tokens[1], tokens[3])
+                offset = line.index(tokens[2])
+                query_seq.addTranslation(previous_line[offset:])
+            elif re.match("^V", line):
+                if seq is None:
+                    seq = Sequence(tokens[5], tokens[4], tokens[6], tokens[3])
+                    align_record.add_v_alignment(seq, astart)
+                else:
+                    seq.addSequence(tokens[5], tokens[4], tokens[6])    
+            elif re.match("^J", line):
+                if jseq is None:
+                    jseq = Sequence(tokens[5], tokens[4], tokens[6], tokens[3])
+                    align_record.add_j_alignment(jseq, astart)
+                else:
+                    jseq.addSequence(tokens[5], tokens[4], tokens[6])    
+        if extractAlign and line.startswith("Lambda"): 
+            extractAlign=False
+            
+        # Cache previous line
+        previous_line = line    
+
+
+def usage():
+    print 'python parseIgBlast.py [--fastq fastq_file] [--blast blastn_input_file] [--mutation mutation_info_outputfile] [--coverage coverage_report_file]'
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ sequtility.py
@@ -0,0 +1,193 @@
+import sys
+
+complement={}
+complement['A'] = 'T'
+complement['T'] = 'A'
+complement['G'] = 'C'
+complement['C'] = 'G'
+complement['-'] = '-'
+complement['N'] = 'N'
+complement['NA'] = 'NA'
+
+bases = ['A','T','C','G']
+
+codontable = {
+'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+'TAC':'Y', 'TAT':'Y', 'TAA':'O', 'TAG':'B',
+'TGC':'C', 'TGT':'C', 'TGA':'J', 'TGG':'W',
+}
+
+
+class Alignment:
+    
+    def __init__(self, name, length):
+        self.name=name
+        self.length = length
+        self.query = None
+        self.vseq = None
+        self.jseq = None
+        
+    def setQuery(self, seq):
+        self.query=seq
+        
+    def hasQuery(self,seq):
+        if seq in self.alignDict:
+            return True
+        return False
+        
+    def add_v_alignment(self, reference, start):
+        self.vseq = reference
+        pad = "".join('-' for i in range(start))
+        self.vseq.addPrefix(pad)
+    
+    def add_j_alignment(self, reference, start):
+        self.jseq = reference
+        pad = "".join('-' for i in range(start))
+        self.jseq.addPrefix(pad)
+     
+    def getCurrentQuery(self):
+        return self.query   
+        
+    def printOut(self):
+        if self.query.getTranslation() is not None:
+            sys.stdout.write(self.query.getTranslation() + "\n")
+        sys.stdout.write(self.query.getSequence() + "\n")
+        if self.vseq is not None:
+            sys.stdout.write(self.vseq.getSequence() + "\n")
+        if self.jseq is not None:
+            sys.stdout.write(self.jseq.getSequence() + "\n")
+                
+    def getMutations(self):
+        mutations = []
+        query = self.query
+        query_seq = query.getSequence()
+        refs = []
+        if self.vseq is not None:
+            refs.append(self.vseq)
+        if self.jseq is not None:
+            refs.append(self.jseq)
+        for seq in refs:
+            rstart = seq.getRange()[0]
+            ref_seq = seq.getSequence()
+            germline = seq.getName()
+            for j in range(len(ref_seq)):
+                # when a mismatch is detected
+                if ref_seq[j] in bases and query_seq[j] in bases:
+                    ref_pos = rstart + seq.getUngappedPosition(j)
+                    # Get the two nucleotides before and after
+                    query_b1="-"
+                    query_b2="-"
+                    query_a1="-"
+                    query_a2="-"
+                    if j >=2:
+                        query_b1 = query_seq[j-1]
+                        query_b2 = query_seq[j-2]
+                    if j < len(query_seq) - 2:
+                        query_a1 = query_seq[j+1]
+                        query_a2 = query_seq[j+2]  
+                    # Get the two nucleotides before and after
+                    ref_b1=query_b1
+                    ref_b2=query_b2
+                    ref_a1=query_a1
+                    ref_a2=query_a2
+                    if j >=2:
+                        if ref_seq[j-1] != ".":
+                            ref_b1 = ref_seq[j-1]
+                        if ref_seq[j-2] != ".":
+                            ref_b2 = ref_seq[j-2]
+                    if j < len(query_seq) - 2:
+                        if ref_seq[j+1] != ".":
+                            ref_a1 = ref_seq[j+1]
+                        if ref_seq[j+2] != ".":
+                            ref_a2 = ref_seq[j+2]     
+                    ref_base = ref_seq[j]
+                    query_base = query_seq[j]
+                    query_position = query.getRange()[0] + query.getUngappedPosition(j)
+                    if query.getTranslation() is not None:
+                        prot = query.getTranslation()
+                        query_codon=""
+                        ref_codon=""
+                        if prot[j] != " ":
+                            query_codon =  query_b1 + query_base + query_a1
+                            ref_codon = ref_b1 + ref_base + ref_a1
+                        elif (j-1)>=0 and prot[j-1] != " ":
+                            query_codon = query_b2 + query_b1 + query_base
+                            ref_codon = ref_b2 + ref_b1 + ref_base
+                        elif (j+1)<len(ref_seq) and prot[j+1] != " ":
+                            query_codon = query_base + query_a1 + query_a2
+                            ref_codon = ref_base + ref_a1 + ref_a2
+                        if query_codon in codontable:
+                            aa = codontable[query_codon]
+                        else:
+                            aa = "-"
+                        if ref_codon in codontable:
+                            ref_aa =  codontable[ref_codon]
+                        else:
+                            ref_aa = "-"
+                    if not query.isStrandPlus():
+                        query_position = self.length - query_position + 1
+                    record=[self.name, germline, query_position, ref_pos, query_base, ref_base, aa, ref_aa]
+                    record.extend([query_b2, query_b1, query_a1, query_a2, ref_b2, ref_b1, ref_a1, ref_a2])
+                    mutations.append(record)           
+        return mutations                
+
+class Sequence:
+    
+    def __init__(self, seq, start, end, name = None, strandPlus=True):
+        self.seq = seq
+        self.start = int(start)
+        self.end = int(end)
+        self.strandPlus=strandPlus
+        self.name = name
+        # if gap=[40,3], it means that there are 3 gaps before position 40 (p<=40)
+        self.gap=[0,0]
+        self.gap[0]=0
+        self.prot=""
+        
+    def getRange(self):
+        return [self.start, self.end]
+    
+    def getSequence(self):
+        return self.seq
+    
+    def getName(self):
+        return self.name
+    
+    def addSequence(self, seq, start, end):
+        if end > self.end:
+            self.end = end
+        self.seq = self.seq + seq
+    
+    def addPrefix(self, seq):
+        self.seq = seq + self.seq        
+    
+    def getUngappedPosition(self, p):
+        ngap = 0
+        for i in range(0, p+1):
+            if self.seq[i] == "-":
+                ngap = ngap + 1          
+        ungap_pos = p - ngap
+        return ungap_pos
+
+    def addTranslation(self, prot):
+        self.prot=self.prot + prot
+
+    def getTranslation(self):
+        return self.prot
+    
+    def isStrandPlus(self):
+        return self.strandPlus
+       
